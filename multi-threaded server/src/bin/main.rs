7e166a46ad2521203a257ef5dcea62399c11de45
Deepak Anand <thedanand@gmail.com>
2024-07-19 03:48:50 +0530

    feat: implemented multi-threaded server

--- multi-threaded server/src/bin/main.rs
+++ multi-threaded server/src/bin/main.rs
@@ -4,13 +4,23 @@ use std::io::prelude::*;
 use std::fs;
 use std::thread;
 use std::time::Duration;
+use multithreadedserver::ThreadPool;
+
+
+
 fn main() {
     let listener = 
         TcpListener::bind("127.0.0.1:7878").unwrap(); 
+
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming(){
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+       
     }
 }
 
@@ -35,7 +45,7 @@ fn handle_connection(mut stream: TcpStream){
 
     };
 
-    let status_line = "HTTP/1.1 404 NOT FOUND";
+    
     let contents = 
         fs::read_to_string(filename).unwrap();
     
